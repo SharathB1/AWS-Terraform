22ab7f1e292be5f6f21fbcbe9d140d1a85095f7b
Sharath Boini <104951203+SharathB1@users.noreply.github.com>
2023-11-22 21:17:52 -0500

    Update outputs.tf

--- modules/EC2/outputs.tf
+++ modules/EC2/outputs.tf
@@ -2,8 +2,3 @@ output "ec2_instance_id" {
   description = "ID of the created EC2 instance."
   value       = aws_instance.example.id
 }
-
-output "ec2_public_ip" {
-  description = "Public IP address of the EC2 instance."
-  value       = aws_instance.example.public_ip
-}
