730595c8afccb0e8a60f634e58985961a5f31fbe
SharathBoini <sharathchandra.b37@gmail.com>
2023-12-10 20:25:41 -0500

    Updating the S3 module

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -4,5 +4,17 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
-  acl    = var.bucket_acl
+}
+
+resource "aws_s3_bucket_ownership_controls" "examplecontrols" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "myacl" {
+  depends_on = [aws_s3_bucket_ownership_controls.examplecontrols]
+  bucket     = aws_s3_bucket.example.id
+  acl        = var.bucket_acl
 }
--- modules/S3/variables.tf
+++ modules/S3/variables.tf
@@ -1,12 +1,13 @@
 variable "aws_region" {
-  description = "AWS region where the S3 bucket will be created."
+  description = "AWS region where the S3 bucket will be created"
+  default     = "us-east-1"
 }
 
 variable "bucket_name" {
-  description = "Name of the S3 bucket."
+  description = "Name of the s3 bucket"
 }
 
 variable "bucket_acl" {
   description = "Access control for the S3 bucket."
-  default     = "private"  # You can change this to "public-read" or other ACL values.
+  default     = "private" # You can change this to "public-read" or other ACL values.
 }
